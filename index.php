6547d207ebafa44ec63b49e1fbb39c62857ea5be
André Fernandes <andrefernandes1885@gmail.com>
2017-05-03 11:15:29 -0300

    Formulários adicionados

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-    $titulos = ["Fossa excelência" , "Senhor" , "Peladinho"];
-    $qualidades = ["autista" , "triste" , "paranoico"];
-    $nome = "Andresinho";
+    session_start();
+
+    $_SESSION['$atividades'][] = $_GET['atividade'];
 
 ?>
 
@@ -10,10 +10,34 @@
 
     <head>
         <meta charset="utf-8">
+        <style>
+            h3{
+                color: aqua;
+            }
+            h2 {
+                color: greenyellow;
+            }
+            html{
+                background-color: black;
+            }
+            ol{
+                color: white;
+            }
+        </style>
     </head>
     <body>
-
-        <h2> Olá <?php echo $titulos[rand(0,2)]." $nome, ".$qualidades[rand(0,2)]; ?></h2>
+        <h2>Lista de Tarefas</h2>
+        <form method="GET">
+        	<input type="text" name="atividade" placeholder="Ex: Levar Bob para passear">
+        	<input type="submit" value="add">
+        </form>
+        <br>
+        <h3>Exibir atividades: </h3>
+            <ol>
+                <?php foreach ($_SESSION['$atividades'] as $ativ):?>
+                <li><?= $ativ; ?></li>
+                <?php endforeach; ?>
+            </ol>
     </body>
 
 </html>
